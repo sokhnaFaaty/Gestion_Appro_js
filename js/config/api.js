b13cfd5f1a7272f607732de2ae11e846d8195f60
nexuusdev <mamefat2004@gmail.com>
2026-06-25 00:03:44 +0200

    Endpoints users

--- js/config/api.js
+++ js/config/api.js
@@ -3,6 +3,6 @@ export const API_BASE_URL = "https://backendappro.onrender.com";
 export const ENDPOINTS = {
   categories: `${API_BASE_URL}/categories`,
   produits: `${API_BASE_URL}/produits`,
-  login: `${API_BASE_URL}/login`,
+  users: `${API_BASE_URL}/users`,
   fournisseurs: `${API_BASE_URL}/fournisseurs`,
 };
